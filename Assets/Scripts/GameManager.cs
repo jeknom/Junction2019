4f14ac915ae0489545023db0634dc5701e2956d2
mahad <36614535+tigereno@users.noreply.github.com>
2019-11-16 00:09:42 +0200

    Generic Setup
    Added some generic scripts.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager :GenericSingletonClass<GameManager> {
+
+
+    // Here create all the states that are going to be handled once like young/old 
+
+    private string stateOfPlayer;
+    public string StateOfPlayer {
+        get {
+            return this.stateOfPlayer;
+        }
+        set {
+            this.stateOfPlayer = value;
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    // Generic scene management
+    public void GoToScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
