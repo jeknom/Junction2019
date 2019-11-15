4969a05be6f25b6d0788f1acb80b82fa11536ca9
Johannes Palvanen <palvanenj@gmail.com>
2019-11-16 00:14:41 +0200

    Initial player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeedModifier = .1f;
+
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+
+        this.transform.position +=
+            new Vector3(horizontalInput, verticalInput) * movementSpeedModifier;
+    }
+}
