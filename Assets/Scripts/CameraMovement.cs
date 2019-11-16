1b3f95aa672886740f584caa103fea62ef22d44c
Johannes Palvanen <palvanenj@gmail.com>
2019-11-16 11:34:06 +0200

    Added the tilemap packages and a first test level

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Basics")]
+    [SerializeField] Transform followTarget;
+    [SerializeField] float xOffset = 0f;
+    [SerializeField] float yOffset = 0f;
+    [SerializeField] float cameraZ = -10f;
+
+    [Header("Follow mode")]
+    [SerializeField] bool useFollowMode = false;
+    [SerializeField] float followSpeedModifier = 0.05f;
+
+    void Update()
+    {
+        var targetX = followTarget.position.x + xOffset;
+        var targetY = followTarget.position.y + yOffset;
+        var targetPosition = new Vector3(targetX, targetY, cameraZ);
+
+        if (useFollowMode)
+        {
+            this.transform.position = Vector3.MoveTowards(
+                this.transform.position,
+                targetPosition,
+                this.followSpeedModifier);
+        }
+        else
+        {
+            this.transform.position = targetPosition;
+        }
+    }
+}
