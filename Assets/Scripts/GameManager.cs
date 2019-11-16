f7b8ca57672e8515e20f20444351301df64989df
mahad <36614535+tigereno@users.noreply.github.com>
2019-11-16 13:35:54 +0200

    :wq

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+public class GameManager : GenericSingletonClass<GameManager> {
 
 public enum PlayerState
 {
@@ -22,11 +23,62 @@ public class GameManager :GenericSingletonClass<GameManager> {
         }
     }
 
+    private bool flip;
+    public bool Flip {
+        get {
+            return this.flip;
+        }
+        set {
+            this.flip = value;
+        }
+    }
+
+    private MainSceneManager mainSceneManager;
+    public MainSceneManager varMainSceneManager {
+        get {
+            return this.mainSceneManager;
+        }
+        set {
+            this.mainSceneManager = value;
+        }
+    }
+
+    private bool timerStarted;
+    public bool TimerStarted {
+        get {
+            return this.timerStarted;
+        }
+        set {
+            this.timerStarted = value;
+        }
+    }
+
     void Start() {
 
     }
 
     void Update() {
+
+        if(!varMainSceneManager) {
+
+            varMainSceneManager = (MainSceneManager)GameObject.FindObjectOfType<MainSceneManager>();
+
+        }
+        else {
+            if(varMainSceneManager.canStart && !timerStarted) {
+                if(flip) {
+
+                    stateOfPlayer = "young";
+                    TimerStarted = true;
+                }
+                else if(!flip) {
+
+                    stateOfPlayer = "old";
+                    TimerStarted = true;
+                }
+            }
+        }
+
 
     }
 
---
+++ Assets/Scripts/MainSceneManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainSceneManager : MonoBehaviour {
+
+    public float timeLimit = 100f;
+    public float timeRemaining;
+
+    public Text timerText;
+    public bool canStart = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = timeLimit;
+        canStart = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.Instance.TimerStarted) {
+
+            StartCountDownTimer();
+
+        }
+    }
+
+
+    public void StartCountDownTimer() {
+
+        if(timeRemaining > 0) {
+
+            timeRemaining -= (Time.deltaTime);
+            timerText.text = "Time: " + timeRemaining;
+
+        }
+        else {
+
+            GameManager.Instance.Flip = !GameManager.Instance.Flip;
+            GameManager.Instance.TimerStarted = false;
+
+            timeRemaining = timeLimit;
+        }
+
+    }
+
+   
+}
