3de638ef1df1834a42eec23aaa374766ab6cf6fa
Johannes Palvanen <palvanenj@gmail.com>
2019-11-16 22:08:25 +0200

    Level finish block

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }    
+    }
+}
