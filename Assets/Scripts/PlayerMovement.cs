670b3c3115d1b108c7fc805e9620ddcf61e0e10e
Johannes Palvanen <palvanenj@gmail.com>
2019-11-17 03:49:42 +0200

    Added character animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,36 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] VariableJoystick joystick;
     [SerializeField] float movementSpeedModifier = 0.1f;
+    PlayerState previousState;
 
     Rigidbody2D rigidbody2d;
+    Animator animator;
 
     private void OnEnable()
     {
         this.rigidbody2d = this.GetComponent<Rigidbody2D>();
+        this.animator = this.GetComponent<Animator>();
+        this.previousState = GameManager.Instance.StateOfPlayer;
     }
 
     void Update()
     {
         var horizontalInput = joystick.Horizontal;
         var verticalInput = joystick.Vertical;
+        var shouldShowWalkAnimation =
+            horizontalInput + verticalInput != 0;
+        this.animator.SetBool("isWalking", shouldShowWalkAnimation);
+
+        var currentState = GameManager.Instance.StateOfPlayer;
+
+        if (currentState != previousState)
+        {
+            var trigger = currentState == PlayerState.OLD
+                ? "triggerOld"
+                : "triggerYoung";
+            this.animator.SetTrigger(trigger);
+            this.previousState = currentState;
+        }
 
         var nextPosition =
             this.transform.position +
